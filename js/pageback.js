440a25cf4330ef3049dd2ceffd09f9a499f090fc
Yuulis <yuulis2005@gmail.com>
2021-08-17 09:45:36 +0900

    [Add] 8 files

---
+++ js/pageback.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    Particles.init({
+        selector: '.background',
+        sizeVariations: 30,
+        color: ['rgb(0, 187, 221)', 'rgba(0, 187, 221, .5)', 'rgba(0, 187, 221, .7)']
+    });
+};
---
+++ js/typedText.js
@@ -0,0 +1,5 @@
+ityped.init(document.querySelector("#ityped"), {
+    strings: ['Here is Yuulis\'s web site!', 'Take a look my works:)'],
+    startDelay: 500,
+    backDelay: 2000
+})
